e3a5650f9f052a105ed067418b0f9ef174b9372c
Marco Andrea <Marco Andrea@LIMO.homenet.telecomitalia.it>
2018-12-25 19:33:09 +0100

    Cambiato condizione per scegliere in cha anno inserire un evento

--- assistenteVocale_googleCalendar/public_html/speech-recognition.js
+++ assistenteVocale_googleCalendar/public_html/speech-recognition.js
@@ -107,11 +107,11 @@ function parseEvent(s)
     {
         var dataAttuale = new Date();
         var mesi = ["gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"];
-        var mese = mesi.indexOf(sElement[indiceOre + 4])+1;
+        var mese = mesi.indexOf(sElement[indiceOre + 4]);
         var giorno = sElement[indiceOre + 3];
         var anno = (mese >= dataAttuale.getMonth() && giorno >= dataAttuale.getDate()) ? dataAttuale.getFullYear() : dataAttuale.getFullYear() + 1;
         console.log(mese+" "+dataAttuale.getMonth());
-        data = anno + "-" + mese + "-" + giorno;
+        data = anno + "-" + (mese+1) + "-" + giorno;
     }
 
     if (summary !== undefined && ora !== undefined && data !== undefined)
