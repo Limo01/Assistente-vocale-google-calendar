10576cba9b169b7081e3759de6df61fa20305976
Marco Andrea <Marco Andrea@LIMO.homenet.telecomitalia.it>
2018-12-28 19:51:22 +0100

    Adesso vengono letti i prossimi 5 eventi

--- assistenteVocale_googleCalendar/public_html/Avatar.js
+++ assistenteVocale_googleCalendar/public_html/Avatar.js
@@ -132,7 +132,7 @@ Avatar.prototype.say= function(messaggio)
     {
         window.avatar.movimentoBocca= setInterval(this.muoviBocca, 200);
         readOutLoud(messaggio);
-        setTimeout(window.avatar.smettiDiParlare, (messaggio.length*90));
+        setTimeout(window.avatar.smettiDiParlare, (messaggio.length*100));
     }
 };
 
